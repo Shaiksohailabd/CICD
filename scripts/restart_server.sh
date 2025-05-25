75c53fd5b29cfe99c4d410550715329a521c1fa8
shaik sohail <shaiksohailabd@gmail.com>
2025-05-25 13:29:55 +0530

    its not apache2 its httpd

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-set -e
 echo "Restarting Apache server..."
-sudo systemctl start apache2
+
+if systemctl list-units --type=service | grep -q httpd.service; then
+  sudo systemctl restart httpd
+elif systemctl list-units --type=service | grep -q apache2.service; then
+  sudo systemctl restart apache2
+else
+  echo "Apache server not installed. Exiting."
+  exit 1
+fi
+
