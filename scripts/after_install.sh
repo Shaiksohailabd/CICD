ba8d38577154397c55a42e7ddf98e115e52b90b5
shaik sohail <shaiksohailabd@gmail.com>
2025-05-25 13:16:24 +0530

    updating the username to apache

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo "Setting file permissions..."
-sudo chown -R www-data:www-data /var/www/html
+sudo chown -R apache:apache /var/www/html
 sudo chmod -R 755 /var/www/html
 echo "Cleaning up old deployment files..."
